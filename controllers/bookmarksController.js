ce873cc0c1611b718ce91724ba49e20fde748831
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-03-11 12:59:09 -0400

    final first day code

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const bookmarks = express.Router();
+const { getAllBookmarks } = require("../queries/bookmarks");
 
-bookmarks.get("/", (req, res) => {
-  res.json({ message: "Get Route" });
+bookmarks.get("/", async (_req, res) => {
+  const allBookmarks = await getAllBookmarks();
+
+  if (allBookmarks[0]) res.status(200).json(allBookmarks);
+  else res.status(500).json({ error: "server error" });
 });
 
 bookmarks.get("/:id", (req, res) => {
